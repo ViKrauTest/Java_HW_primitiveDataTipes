34ba497c6334d0910ebf10a529b7c1f9ad758923
Viktoria <vikamosk@gmail.com>
2022-06-20 22:23:17 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 2563;
+        int mileCost = 20;
+
+        int miles = ticketPrice / mileCost;
+        System.out.println("Начисленные мили:  " + miles);
+    }
+}
